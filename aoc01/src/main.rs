6cb49aad5e6cc64adee7eaedbb76e0dfbc175ffc
Drew Walker <drewiwalker@gmail.com>
2024-12-01 16:49:42 -0500

    refactor part 1 to use iterators

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -4,9 +4,10 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let input_path = args.get(1).expect( "must provide file path!");
 
-    let (left, right) = utils::left_and_right_lists(input_path);
-    let iter = left.iter().zip(right.iter());
+    let (mut left, mut right) = utils::left_and_right_lists(input_path);
 
+    left.sort();
+    right.sort();
     let dif: i32 = left.iter().zip(right.iter())
         .map(|(l, r)| l - r)
         .map(i32::abs)
