f1100c3c1e45b886c9d150b9e0d6de838b512b93
Drew Walker <drewiwalker@gmail.com>
2024-12-01 21:52:37 -0500

    use a map for problem 2

--- aoc02/src/main.rs
+++ aoc02/src/main.rs
@@ -1,26 +1,22 @@
+use std::collections::HashMap;
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let input_path = args.get(1).expect( "must provide file path!");
 
-    let (mut left, mut right) = utils::left_and_right_lists(input_path);
+    let (left, right) = utils::left_and_right_lists(input_path);
+
+    let mut right_counts = HashMap::new();
 
     let mut similarity = 0;
 
-    left.sort();
-    right.sort();
-    let mut left_idx = 0;
-    let mut right_idx = 0;
-    while left_idx < left.len() {
-        let left_val = left[left_idx];
-        let mut times_matched = 0;
-        while right_idx < right.len() && right[right_idx] <= left_val  {
-            if right[right_idx] == left_val {
-                times_matched += 1;
-            }
-            right_idx += 1;
-        }
-        similarity = similarity + (left_val * times_matched);
-        left_idx += 1;
+    for right_val in right {
+        let count = right_counts.entry(right_val).or_insert(0);
+        *count += 1;
     }
+    for left_val in left {
+        similarity += left_val * right_counts.get(&left_val).unwrap_or(&0);
+    }
+
     print!("{}", similarity);
 }
